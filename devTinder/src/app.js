e6776ff2a86fc29e8471a6befc2a6126abb9a128
hasuq33 <hardworkerarmy1234@gmail.com>
2024-12-29 18:19:50 +0530

    [IMP] Diving into the APIS

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -4,15 +4,12 @@ const { adminAuth , userAuth} = require("./middlewares/auth")
 const app = express(); 
 const User = require("./models/user.model"); 
 
-app.post("/signup",async (req, res)=>{
+// Here we use express.json middleware to parse the json Data
+app.use(express.json());
 
+app.post("/signup",async (req, res)=>{
     // Creating a new instance of the User Model
-    const user = new User({
-        firstName: "Harshiv", 
-        lastName: "Joshi",
-        emailId: "harshivjoshi1234@gamil.com",
-        password:"Harshi10",
-    })
+    const user = new User(req.body)
 try {
     await user.save();
     res.send("User added successfully!");
@@ -20,6 +17,63 @@ try {
     res.status(400).send("Error saving the User: "+ error.message);
 }
     
+}); 
+
+// Get User by email
+app.get("/user", async (req, res)=>{
+    const userID = req.body.id; 
+    try {
+        const user = await User.findById(userID);
+        console.log(user)
+        if (user) res.send(user);
+        else res.status(404).send("User not Found!")
+    } catch (error) {
+        res.status(400).send("Something went wrong!")
+    }
+})
+
+// Get all Data from DB
+app.get("/feed",async (req, res)=>{
+    try {
+        const users = await User.find({}); 
+        if (users.length > 0) res.send(users);
+        else res.status(404).send("User not Found!")
+    } catch (error) {
+        res.status(400).send("Something went wrong!");
+    }
+})
+
+// Delete User By it's ID
+app.delete("/user",async (req , res)=>{
+    const userID = req.body.id;
+    console.log(userID);
+    if(!userID) res.status(404).send("Please enter a user ID");
+    try {
+        const user = await  User.findById(userID);
+        if(user){
+            await User.findByIdAndDelete(userID);
+            res.status(200).send("User deleted successfully")
+        }else{
+            res.status(404).send("User not found");
+        }
+        
+    } catch (error) {
+        console.log("Error: " + error.message);
+    }
+})
+
+// Update User Documents
+app.patch("/user",async (req , res)=>{
+    const userID = req.body.id;
+    console.log(userID);
+    if(!userID) res.status(404).send("Please enter a user ID");
+    try {
+        const user = await User.findByIdAndUpdate(userID,req.body);
+        res.send("User updated successfully updated");
+        
+    } catch (error) {
+        console.log("Error: " + error.message);
+    }
 })
 
 connectDB().then(()=>{
